be3dc9e90b5f2a65ce39d2ae8c7413c6f7d91b81
JB-uses-git <jaladbhairav71@gmail.com>
2026-09-15 15:42:35 +0530

    Step 6: Quick Settings Tile - TileService to toggle NetShift watcher on/off from quick settings

---
+++ app/src/main/java/com/jb/netshift/NetShiftTileService.kt
@@ -0,0 +1,41 @@
+package com.jb.netshift
+
+import android.content.Intent
+import android.graphics.drawable.Icon
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+
+class NetShiftTileService : TileService() {
+
+    override fun onStartListening() {
+        super.onStartListening()
+        updateTileState()
+    }
+
+    override fun onClick() {
+        super.onClick()
+        if (NetworkWatchService.isRunning) {
+            stopService(Intent(this, NetworkWatchService::class.java))
+        } else {
+            startForegroundService(Intent(this, NetworkWatchService::class.java))
+        }
+        updateTileState()
+    }
+
+    private fun updateTileState() {
+        val tile = qsTile ?: return
+        val isRunning = NetworkWatchService.isRunning
+
+        if (isRunning) {
+            tile.state = Tile.STATE_ACTIVE
+            tile.label = "NetShift"
+            tile.subtitle = "Monitoring"
+        } else {
+            tile.state = Tile.STATE_INACTIVE
+            tile.label = "NetShift"
+            tile.subtitle = "Off"
+        }
+        tile.icon = Icon.createWithResource(this, R.drawable.ic_tile)
+        tile.updateTile()
+    }
+}
